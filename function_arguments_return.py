77067b551dbca66bbae6ad171e0986f050c4972b
unknown <manaladbryantray_bsit@plmun.edu.ph>
2025-02-22 15:48:28 -0700

    Submitting Python Week 04 - Laboratory # 17

---
+++ function_arguments_return.py
@@ -0,0 +1,17 @@
+# BRYANT RAY M. MANALAD
+# ITELEC2
+# Laboratory #17 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def main():
+    import math
+    def circle_area(radius):
+        area = math.pi * (radius ** 2)
+        return area
+    radius_value = 5
+    area_result = circle_area(radius_value)
+    print(f"The area of a circle with radius {radius_value} is: {area_result:.2f}")
+
+if _name_ == "_main_":
+    main()
+    
